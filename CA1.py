0297705e44d9040f155b72473304de0fe823e7e3
areich128 <alex.reich128@gmail.com>
2023-11-11 10:10:15 -0700

    added comments with lenia algo, and defined a convolve function in CA1

--- CA1.py
+++ CA1.py
@@ -29,30 +29,32 @@ array_2d = garray_2d / 255.0
 
 # array_2d = np.random.random((100,100))
 
-kernel = np.array([
+kernel1 = np.array([
     [-1, 0, 1],
     [-1, 0, 1],
     [-1, 0, 1]
 ])
 
-result = np.zeros_like(array_2d)
 
-for i in range(1, array_2d.shape[0] - 1):
-    for j in range(1, array_2d.shape[1] - 1):
-        # working on wrap-around function here
-        result[i][j] = np.sum(array_2d[i-1:i+2, j-1:j+2] * kernel)
+def convolve(array_2d, kernel):
+    result = np.zeros_like(array_2d)
+    for i in range(1, array_2d.shape[0] - 1):
+        for j in range(1, array_2d.shape[1] - 1):
+            # working on wrap-around function here
+            result[i][j] = np.sum(array_2d[i-1:i+2, j-1:j+2] * kernel)
+    return result
 
 
 plt.subplot(1, 3, 1)
-plt.imshow(kernel, cmap='Grays')
+plt.imshow(kernel1, cmap='Greys')
 plt.colorbar()
 
 plt.subplot(1, 3, 2)
-plt.imshow(array_2d, cmap='Grays')
+plt.imshow(array_2d, cmap='Greys')
 plt.colorbar()
 
 plt.subplot(1, 3, 3)
-plt.imshow(result, cmap='Grays')
+plt.imshow(convolve(array_2d, kernel1), cmap='Greys')
 plt.colorbar()
 plt.show()
 
---
+++ LeniaImpl1.py
@@ -0,0 +1,8 @@
+# LENIA ALGORITHM
+# 1. initialize world (2d array)
+# 2. Convolve with Lenia Kernel
+# 3. Apply a growth mapping function to the weighted sums (G is uni-modal, standard dist type)
+# 4. Add a small portion (dt) of values back to A (world array)
+# 5. Clip A to between 0 and 1
+# 6. Repeat 2-5 for each time step
+
